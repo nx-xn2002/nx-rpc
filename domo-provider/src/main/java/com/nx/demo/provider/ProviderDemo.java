d8992d6be4e045fba1b851eee497d95ba045801c
nx-xn2002 <1870230468@qq.com>
2024-12-28 17:32:49 +0800

    write demo provider

---
+++ domo-provider/src/main/java/com/nx/demo/provider/ProviderDemo.java
@@ -0,0 +1,10 @@
+package com.nx.demo.provider;
+
+/**
+ * provider demo
+ *
+ * @author nx-xn2002
+ * @date 2024-12-28
+ */
+public class ProviderDemo {
+}
---
+++ domo-provider/src/main/java/com/nx/demo/provider/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.nx.demo.provider.service;
+
+import com.nx.demo.common.model.User;
+import com.nx.demo.common.service.UserService;
+
+/**
+ * 用户服务实现类
+ *
+ * @author nx-xn2002
+ * @date 2024-12-28
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUser(User user) {
+        return null;
+    }
+}
